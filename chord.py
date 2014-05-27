301bdee7b94f177f5b032a13c42b698e2a310552
neil <neil@neil-comp.local>
2014-05-27 13:35:42 -0300

    split X and N classifications

--- chord.py
+++ chord.py
@@ -2,64 +2,64 @@
 
 class Chord:
   # in ascending semitone order
-  validRoots = [
-    ['A'],
-    ['A#', 'Bb'],
-    ['B', 'Cb'],
-    ['C'],
-    ['C#', 'Db'],
-    ['D'],
-    ['D#', 'Eb'],
-    ['E', 'Fb'],
-    ['F'],
-    ['F#', 'Gb'],
-    ['G'],
-    ['G#', 'Ab'],
-  ]
-  emptyChordStrings = ['N', 'X']
+  # roots in the same subarray are considererd equivalent
+  validRoots = {
+    'standard': [
+      ['A'],
+      ['A#', 'Bb'],
+      ['B', 'Cb'],
+      ['C'],
+      ['C#', 'Db'],
+      ['D'],
+      ['D#', 'Eb'],
+      ['E', 'Fb'],
+      ['F'],
+      ['F#', 'Gb'],
+      ['G'],
+      ['G#', 'Ab'],
+    ],
+    'nonstandard': [
+      ['N'],
+      ['X'],
+    ]
+  }
 
   def __init__(self, chordString):
+    self.rootClass = None # 'standard' or 'nonstandard'
     self.rootIndex = None # left side of : (eg D#)
     self.quality = None # right side of : (eg min7)
 
     chordString = chordString.split(':')
 
-    # handle "empty" note case
-    if chordString[0] in Chord.emptyChordStrings:
-      self.rootIndex = -1
-      self.quality = 'None'
-    # handle "regular" note case
-    else: 
-      if len(chordString) != 2:
-        print chordString
-        raise Exception('bad chordString')
+    if len(chordString) != 2:
+      print chordString
+      raise Exception('bad chordString')
 
-      # search the validRoots for anything that matches the input string
-      for (index, roots) in enumerate(Chord.validRoots):
+    # search the validRoots for anything that matches the input string
+    for rootClass in Chord.validRoots.keys():
+      for (index, roots) in enumerate(Chord.validRoots[rootClass]):
         for root in roots:
           if chordString[0] == root:
             self.rootIndex = index
+            self.rootClass = rootClass
 
-      # if couldn't find anything in valid roots
-      if self.rootIndex == None:
-        raise Exception('bad chordString')
+    # if couldn't find anything in valid roots
+    if self.rootIndex == None:
+      raise Exception('bad chordString')
 
-      self.quality = chordString[1]
+    self.quality = chordString[1]
 
   def __str__(self):
-    if self.rootIndex == -1:
-      return Chord.emptyChordStrings[0]
-    else:
-      return Chord.validRoots[self.rootIndex][0] + ':' + self.quality
+    return Chord.validRoots[self.rootClass][self.rootIndex][0] + ':' + self.quality
 
   def __eq__(self, other):
     return str(self) == str(other)
 
   def transposed(self, numSemitones):
-    # don't transpose if the note is empty
     newChord = Chord(str(self))
-    if newChord.rootIndex != -1:
-      newChord.rootIndex = (newChord.rootIndex + numSemitones) % len(Chord.validRoots)
+    # don't transpose if the note is nonstandard (eg 'X' or 'N')
+    if self.rootClass == 'standard':
+      newChord.rootIndex = (newChord.rootIndex + numSemitones) % len(Chord.validRoots[self.rootClass])
     return newChord
 
 # some quick unit tests
@@ -67,6 +67,7 @@ if __name__ == '__main__':
   assert Chord('Bb:maj') == Chord('A#:maj')
   assert Chord('F:7').transposed(1) == Chord('F#:7')
   assert Chord('G:7').transposed(11) == Chord('F#:7')
+  assert Chord('X:7') != Chord('A#:7')
   print 'All tests passed'
 
 
